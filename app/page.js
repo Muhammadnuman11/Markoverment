60e9f318c85c5b71cec32c83858e1db53d150350
Numan Irshad <80625784+Muhammadnuman11@users.noreply.github.com>
2024-04-04 17:39:20 +0500

    home page

--- app/page.js
+++ app/page.js
@@ -3,6 +3,10 @@ import Image from "next/image";
 import { ReactTyped } from "react-typed";
 import Clouds from "../public/header/Clouds.png";
 import Logo from "../public/header/Logo.png";
+import BuildingConnections from "../public/icons/building-connections.png";
+import Innovation from "../public/icons/Innovation.png";
+import Complex from "../public/icons/complex-solutions.png";
+import Satisfuction from "../public/icons/customer-satisfuction.png";
 import Name from "../public/header/Name.png";
 import HomeImg from "../public/header/home.png";
 
@@ -35,9 +39,6 @@ export default function Home() {
               <p className="mt-2 md:mt-3 text-justify w-full lg:w-9/10">
                 Markoverment delivers top-notch digital solutions, encompassing Social Media Marketing, Digital Marketing, SEO, Shopify Development, Web Development, and Graphic Designing. Committed to excellence and innovation, we empower businesses to flourish in the digital realm. With our comprehensive suite of services, we cater to diverse needs and ensure your online success.
               </p>
-              {/* <p className="mt-2 md:mt-3 text-center w-full lg:w-9/10">Additionally, our proficiency in Shopify Development and Web Development ensures that businesses have seamless, user-friendly online experiences tailored to their specific requirements. At Markoverment, we are committed to delivering innovative solutions that empower our clients to succeed in today's digital landscape.
-              </p> */}
-              {/* <button className="w-40 py-2 mt-3 md:mt-6 font-bold border-2 border-[#fbc245] rounded-full cursor-pointer hover:bg-[#fbc245]">Start</button> */}
             </div>
           </div>
           <div className="h-full w-full md:w-1/2 flex justify-end items-center flex-col">
@@ -48,6 +49,180 @@ export default function Home() {
           </div>
         </div>
 
+        {/* Why Choose Us  */}
+        <div>
+          <section id="features" className="relative block px-6 py-10 md:py-20 md:px-10">
+            <div className="relative mx-auto max-w-5xl text-center">
+              <span className="text-gray-400 my-3 flex items-center justify-center font-medium uppercase tracking-wider">
+                Why choose us
+              </span>
+              <h2
+                className="block w-full bg-gradient-to-b from-white to-gray-400 bg-clip-text font-bold text-transparent text-3xl sm:text-4xl">
+                Build a Website That Your Customers Love
+              </h2>
+              <p
+                className="mx-auto my-4 w-full max-w-xl bg-transparent text-center font-medium leading-relaxed tracking-wide text-gray-400">
+                Our templates allow for maximum customization. No technical skills required - our intuitive design tools
+                let
+                you get the job done easily.
+              </p>
+            </div>
+
+
+            <div className="relative mx-auto max-w-7xl z-10 grid grid-cols-1 gap-10 md:gap-4 pt-14 sm:grid-cols-2 lg:grid-cols-4">
+
+              <div className="rounded-md border border-neutral-800 bg-neutral-900/50 p-4 text-center shadow">
+                <div className="button-text mx-auto flex h-12 w-12 items-center justify-center rounded-md bg-[#fbbb37]">
+                  <Image src={BuildingConnections} width={30} priority={true} alt="Building Connections" />
+                </div>
+                <h3 className="mt-6 text-gray-400 text-xl">Building Connections</h3>
+                <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400">We believe in building connections with the people either they are buyers or audiences.
+                </p>
+              </div>
+
+              <div className="rounded-md border border-neutral-800 bg-neutral-900/50 p-4 text-center shadow">
+                <div className="button-text mx-auto flex h-12 w-12 items-center justify-center rounded-md bg-[#fbbb37]">
+                  <Image src={Innovation} width={30} priority={true} alt="Innovation" />
+                </div>
+                <h3 className="mt-6 text-gray-400 text-xl">Innovation at its best</h3>
+                <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400">The professionals of Markoverment are not only creative but maintaining the uniqueness from planning to designing.
+                </p>
+              </div>
+
+              <div className="rounded-md border border-neutral-800 bg-neutral-900/50 p-4 text-center shadow">
+                <div className="button-text mx-auto flex h-12 w-12 items-center justify-center rounded-md bg-[#fbbb37]">
+                  <Image src={Complex} width={30} priority={true} alt="Complex solutions" />
+                </div>
+                <h3 className="mt-6 text-gray-400 text-xl">Complex solutions</h3>
+                <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400">
+                  Nothing is here impossible for the team. They are determined to walk the difference roads to reach the heights of quality.
+                </p>
+              </div>
+
+              <div className="rounded-md border border-neutral-800 bg-neutral-900/50 p-4 text-center shadow">
+                <div className="button-text mx-auto flex h-12 w-12 items-center justify-center rounded-md bg-[#fbbb37]">
+                  <Image src={Satisfuction} width={30} priority={true} alt="Satisfuction" />
+                </div>
+                <h3 className="mt-6 text-gray-400 text-xl">Customer Satisfaction</h3>
+                <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400">
+                  Customer satisfaction is our top priority.
+                </p>
+              </div>
+
+            </div>
+
+            {/* <div className="absolute bottom-0 left-0 z-0 h-1/3 w-full border-b"
+              style={{ backgroundImage: 'linear-gradient(to right top, rgba(79, 70, 229, 0.2) 0%, transparent 50%, transparent 100%)', borderColor: 'rgba(92, 79, 240, 0.2)' }}>
+            </div>
+            <div className="absolute bottom-0 right-0 z-0 h-1/3 w-full"
+              style={{ backgroundImage: 'linear-gradient(to left top, rgba(220, 38, 38, 0.2) 0%, transparent 50%, transparent 100%)', borderColor: 'rgba(92, 79, 240, 0.2)' }}>
+            </div> */}
+          </section>
+        </div>
+
+        {/* Our Process */}
+        <section className="py-12 sm:py-16 lg:py-20 xl:py-24">
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="text-center">
+              <p className="text-gray-400 my-3 flex items-center justify-center font-medium uppercase tracking-wider">Our Process</p>
+              <h2 className="block w-full bg-gradient-to-b from-white to-gray-400 bg-clip-text font-bold text-transparent text-3xl sm:text-4xl">Launch your blog
+                in
+                4 easy steps
+              </h2>
+              <p className="mx-auto my-4 w-full max-w-xl bg-transparent text-center font-medium leading-relaxed tracking-wide text-gray-400">
+                Create your own blog with us and launch it with just 4 easy steps
+              </p>
+            </div>
+            <ul className="mx-auto mt-12 grid max-w-md grid-cols-1 gap-4 sm:mt-16 lg:mt-20 lg:max-w-full lg:grid-cols-5">
+
+              <li className="flex-start group relative flex lg:flex-col">
+                <span
+                  className="absolute left-[18px] top-14 h-[calc(100%_-_32px)] w-px bg-gray-300 lg:right-0 lg:left-auto lg:top-[18px] lg:h-px lg:w-[calc(100%_-_72px)]"
+                  aria-hidden="true"></span>
+                <div
+                  className="inline-flex h-10 w-10 shrink-0 items-center justify-center rounded-md transition-all duration-200 bg-[#fbbb37] font-semibold">
+                  01
+                </div>
+                <div className="ml-6 lg:ml-0 lg:mt-2">
+                  <h3
+                    className="mt-6 text-gray-400 text-xl">
+                    Connect
+                  </h3>
+                  <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400">Get in touch with our team and let the brilliance of our experts be a savior for your business.</p>
+                </div>
+              </li>
+
+              <li className="flex-start group relative flex lg:flex-col">
+                <span
+                  className="absolute left-[18px] top-14 h-[calc(100%_-_32px)] w-px bg-gray-300 lg:right-0 lg:left-auto lg:top-[18px] lg:h-px lg:w-[calc(100%_-_72px)]"
+                  aria-hidden="true"></span>
+                <div
+                  className="inline-flex h-10 w-10 shrink-0 items-center justify-center rounded-md transition-all duration-200 bg-[#fbbb37] font-semibold">
+                  02
+                </div>
+                <div className="ml-6 lg:ml-0 lg:mt-2">
+                  <h3
+                    className="mt-6 text-gray-400 text-xl">
+                    Discuss your idea
+                  </h3>
+                  <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400">Share your idea, discuss requirements and prepare a modal of the project, to begin with the strategy building phase.</p>
+                </div>
+              </li>
+
+              <li className="flex-start group relative flex lg:flex-col">
+                <span
+                  className="absolute left-[18px] top-14 h-[calc(100%_-_32px)] w-px bg-gray-300 lg:right-0 lg:left-auto lg:top-[18px] lg:h-px lg:w-[calc(100%_-_72px)]"
+                  aria-hidden="true"></span>
+                <div
+                  className="inline-flex h-10 w-10 shrink-0 items-center justify-center rounded-md transition-all duration-200 bg-[#fbbb37] font-semibold">
+                  03
+                </div>
+                <div className="ml-6 lg:ml-0 lg:mt-2">
+                  <h3
+                    className="mt-6 text-gray-400 text-xl">
+                    Planning strategies
+                  </h3>
+                  <p className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400">Discuss your requirements with our creative team and start building strategies to move forward to the development phase.</p>
+                </div>
+              </li>
+
+              <li className="flex-start group relative flex lg:flex-col">
+                <span
+                  className="absolute left-[18px] top-14 h-[calc(100%_-_32px)] w-px bg-gray-300 lg:right-0 lg:left-auto lg:top-[18px] lg:h-px lg:w-[calc(100%_-_72px)]"
+                  aria-hidden="true"></span>
+                <div
+                  className="inline-flex h-10 w-10 shrink-0 items-center justify-center rounded-md transition-all duration-200 bg-[#fbbb37] font-semibold">
+                  04
+                </div>
+                <div className="ml-6 lg:ml-0 lg:mt-2">
+                  <h3
+                    className="mt-6 text-gray-400 text-xl">
+                    Implementation
+                  </h3>
+                  <h4 className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400">Once the team has built strategies, the experts will start implementing them after yuor approval.</h4>
+                </div>
+              </li>
+
+              <li className="flex-start group relative flex lg:flex-col">
+                <span
+                  className="absolute left-[18px] top-14 h-[calc(100%_-_32px)] w-px bg-gray-300 lg:right-0 lg:left-auto lg:top-[18px] lg:h-px lg:w-[calc(100%_-_72px)]"
+                  aria-hidden="true"></span>
+                <div
+                  className="inline-flex h-10 w-10 shrink-0 items-center justify-center rounded-md transition-all duration-200 bg-[#fbbb37] font-semibold">
+                  05
+                </div>
+                <div className="ml-6 lg:ml-0 lg:mt-2">
+                  <h3
+                    className="mt-6 text-gray-400 text-xl">
+                    Results
+                  </h3>
+                  <h4 className="my-4 mb-0 font-normal leading-relaxed tracking-wide text-gray-400">We stay connected with the customers during the development phase and enjoy the desired results together.</h4>
+                </div>
+              </li>
+
+            </ul>
+          </div>
+        </section>
       </div>
 
     </>
--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -2,6 +2,10 @@ import Link from 'next/link'
 import React from 'react'
 import Logo from "../../public/header/Logo.png";
 import Name from "../../public/header/Name.png";
+import Facebook from "../../public/icons/facebook.png";
+import Insta from "../../public/icons/insta.png";
+import Twitter from "../../public/icons/twitter.png";
+import Linkedin from "../../public/icons/linkedin.png";
 import Image from 'next/image';
 
 export default function Footer() {
@@ -10,12 +14,12 @@ export default function Footer() {
         <>
             <footer className="shadow">
                 <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
-                    <div className="sm:flex sm:items-center sm:justify-between">
-                        <Link href="/" className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse" >
-                            <Image src={Logo} width={50} priority={true} alt="Company Logo"/>
+                    <div className="flex items-center justify-between flex-wrap">
+                        <Link href="/" className="flex items-center mb-4 md:mb-0 space-x-3 rtl:space-x-reverse" >
+                            <Image src={Logo} width={50} priority={true} alt="Company Logo" />
                             <Image src={Name} width={200} priority={true} alt="Company Name" className='' />
                         </Link>
-                        <ul className="flex flex-wrap items-center mb-6 text-sm font-medium sm:mb-0 text-white">
+                        <ul className="flex items-center mb-4 text-sm font-medium md:mb-0 text-white">
                             <li>
                                 <Link href="/" className="hover:underline me-4 md:me-6">About</Link>
                             </li>
@@ -29,6 +33,23 @@ export default function Footer() {
                                 <Link href="/" className="hover:underline">Contact</Link>
                             </li>
                         </ul>
+                        <div className="">
+                            <h5 className='text-white text-md font-bold mb-2'>Follow Us</h5>
+                            <div className='flex gap-8'>
+                                <Link href="https://www.facebook.com/markoverment" target="_blank" rel="noopener noreferrer">
+                                    <Image src={Facebook} width={20} priority={true} className='invert' alt="Facebook" />
+                                </Link>
+                                <Link href="https://www.instagram.com/markoverment" target="_blank" rel="noopener noreferrer">
+                                    <Image src={Insta} width={20} priority={true} className='invert' alt="Instagram" />
+                                </Link>
+                                <Link href="https://www.twitter.com/markoverment" target="_blank" rel="noopener noreferrer">
+                                    <Image src={Twitter} width={20} priority={true} className='invert' alt="twitter" />
+                                </Link>
+                                <Link href="https://www.linkedin.com/company/markoverment" target="_blank" rel="noopener noreferrer">
+                                    <Image src={Linkedin} width={20} priority={true} className='invert' alt="linkedin" />
+                                </Link>
+                            </div>
+                        </div>
                     </div>
                     <hr className="border-white sm:mx-auto my-6" />
                     <span className="block text-sm text-white sm:text-center">&copy; {years} <Link href="/" className="hover:underline">Markoverment</Link>. All Rights Reserved.</span>
